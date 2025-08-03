82500081f22b309eb4a31507be7b349e1e3cfcd0
taichi3012 <68967462+taichi3012@users.noreply.github.com>
2025-08-04 00:48:10 +0900

    [Change]ワールドロード時のメッセージを変更

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -6,7 +6,7 @@ import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.IChatComponent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -29,15 +29,17 @@ public class ExampleMod {
   private static final String TEMPLATE_REPO_URL = "https://github.com/taichi3012/Forge1.8.9DevTemplate";
 
   @SubscribeEvent
-  public void onWorldLoad(WorldEvent.Load event) {
-    printChatMessage(
-      new ChatComponentText("§b§lこのModは、")
-        .appendSibling(
-          new ChatComponentText("§c§l§ntaichi3012/Forge1.8.9DevTemplate")
-            .setChatStyle(new ChatStyle().setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, TEMPLATE_REPO_URL)))
-        )
-        .appendText("§b§lを使用して作成されました。")
-    );
+  public void onWorldLoad(EntityJoinWorldEvent event) {
+    if (event.entity == Minecraft.getMinecraft().thePlayer) {
+      printChatMessage(
+        new ChatComponentText("§b§lこんにちは" + event.entity.getName() + "さん。このModは、")
+          .appendSibling(
+            new ChatComponentText("§c§l§ntaichi3012/Forge1.8.9DevTemplate")
+              .setChatStyle(new ChatStyle().setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, TEMPLATE_REPO_URL)))
+          )
+          .appendText("§b§lを使用して作成されました。")
+      );
+    }
   }
 
   public static void printChatMessage(IChatComponent component) {
